0f386e413e7cb3c747e6cd9f22ab23f3d5a07bfb
Aleksashka301 <aleksashka301@gmail.com>
2025-01-30 17:26:14 +0500

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -2,7 +2,7 @@ import os
 import django
 import argparse
 import random
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
@@ -13,16 +13,11 @@ from datacenter.models import Schoolkid, Mark, Chastisement, Commendation
 
 
 def change_ratings(name_student):
-    student_marks = Mark.objects.filter(schoolkid=name_student, points__in=[2, 3])
-    for mark in student_marks:
-        mark.points = random.choice([4, 5])
-        mark.save()
+    student_marks = Mark.objects.filter(schoolkid=name_student, points__in=[2, 3]).update(points=5)
 
 
 def delete_comments(name_student):
-    comments_bad = Chastisement.objects.filter(schoolkid=name_student)
-    for comment in comments_bad:
-        comment.delete()
+    comments_bad = Chastisement.objects.filter(schoolkid=name_student).delete()
 
 
 def add_comment(name_student, subject, commentary):
@@ -31,8 +26,12 @@ def add_comment(name_student, subject, commentary):
         subject__title=subject,
         points=5
     )
-    last_mark = last_mark.order_by('-created').first()
 
+    if not last_mark:
+        print('По данному предмету нет оценок со значением 5.')
+        return
+
+    last_mark = last_mark.order_by('-created').first()
     teacher = last_mark.teacher
     created_date = last_mark.created
     subject = last_mark.subject
@@ -66,4 +65,5 @@ if __name__ == '__main__':
         print(f'Ученика с именем {name_student} в базе данных нет, либо вы ввели имя не полностью.')
     except AttributeError:
         print(f'Предмета {subject} не существует либо он записан не верно.')
-
+    except MultipleObjectsReturned:
+        print('Выбрано более одного ученика! Введите имя полностью.')
